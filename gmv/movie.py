e89ee60ebeb5b90ade2cabaea2c7cfd556445b20
WST <admin@jsmart.web.id>
2014-03-22 16:09:44 +0400

    Fixes

--- gmv/movie.py
+++ gmv/movie.py
@@ -2,18 +2,19 @@
 # (C) 2014 Ilya Averkov (WST)
 # http://ilya.averkov.net
 
+from struct import unpack
+
 class Movie:
 	def __init__(self, filename):
-		f = open(filename, 'r')
-		header = f.read(63)
-		if not header.startswith('Gens Movie TEST'):
-			raise Exception("The file is not a valid Gens movie file")
+		with open(filename, 'r') as f:
+			header = f.read(64)
+			if not header.startswith('Gens Movie TEST'):
+				raise Exception("The file is not a valid Gens movie file")
 
-		self.header = struct.unpack('15scib2bb40s', header)
-		f.close()
+			self.header = unpack('15scib2bb40s', header)
 
 	def rerecords(self):
 		return self.header[2]
 
 	def comment(self):
-		return self.header[7]
+		return self.header[7].replace('\x00', '')
