5e54a1e078545666952f183a612b85d8202a583b
WST <admin@jsmart.web.id>
2014-03-22 15:46:47 +0400

    Distutils setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+	name = 'Gens Movie support for Python',
+	version = '0.1.0',
+	author = 'Ilya I. Averkov (WST)',
+	author_email = 'admin@jsmart.web.id',
+	packages = ['gmv'],
+	url = 'http://github.com/WST/python-gmv'
+)
